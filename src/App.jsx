efef8bb84a2788d4735cc25a9f9015b763e08621
GolbanEF <Golban.ef@yandex.ru>
2024-04-20 18:55:14 +0300

    go

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card 
+        title={'Card title'}
+        text={"Some quick example text to build on the card title and make up the bulk of the card's content."}
+      >
+        <img src="./img2.jpeg" className="card-img-top" alt="card_img"/>
+      </Card>
+      <Card 
+        title={'Special title treatment'}
+        text={'With supporting text below as a natural lead-in to additional content.'}
+        />
+    </div>
+  );
+}
+
+export default App;
